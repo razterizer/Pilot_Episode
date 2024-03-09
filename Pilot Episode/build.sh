7a95eda90fb16b9f7538187865b1e3caf34773aa
Rasmus <scratty@gmail.com>
2024-03-09 07:30:55 +0100

    build.sh: * Adding compiler flags for linking with OpenAL.

--- Pilot Episode/build.sh
+++ Pilot Episode/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-../../lib/Core\ Lib/build.sh pilot_episode $1
+additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal"
+#additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal.1.23.1"
+#additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib /opt/homebrew/opt/openal-soft/lib/libopenal.dylib"
+
+
+../../lib/Core\ Lib/build.sh pilot_episode $1 "${additional_flags[@]}"
