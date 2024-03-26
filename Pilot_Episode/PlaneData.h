cf3b9650cc35cb1190f2b281c880aab1d4b090a9
razterizer <scratty@gmail.com>
2024-03-26 14:41:45 +0100

    Revert "Updating include paths to use submodule paths instead."
    This reverts commit e5c09fec88720c28452cf38713e13dfc0cc9221c.

--- Pilot_Episode/PlaneData.h
+++ Pilot_Episode/PlaneData.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "Termin8or/Screen.h"
-#include "Termin8or/ParticleSystem.h"
+#include "../../lib/Termin8or/Screen.h"
+#include "../../lib/Termin8or/ParticleSystem.h"
 
 namespace plane_data
 {
