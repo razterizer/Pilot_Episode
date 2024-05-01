ce2504140fcfae73b5d74fa2b4fac45117177852
Thibaut Buchert <thibaut.buchert@gmail.com>
2024-05-01 19:06:49 +0200

    Find openal build flags using pkg-config

--- Pilot_Episode/build.sh
+++ Pilot_Episode/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal"
+additional_flags=""
 #additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal.1.23.1"
 #additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib /opt/homebrew/opt/openal-soft/lib/libopenal.dylib"
 
+export BUILD_PKG_CONFIG_MODULES='openal'
 
-../../lib/Core/build.sh pilot_episode $1 "${additional_flags[@]}"
+../../lib/Core/build.sh pilot_episode "$1" "${additional_flags[@]}"
