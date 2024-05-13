05a83846dc6353917f475b33b427ad96cf6b0823
Rasmus Anthin <scratty@gmail.com>
2024-05-13 17:58:21 +0200

    build.sh: * Instead of just setting additional_flags = "", rather check what OS we're running on and if on MacOS (Darwin), then we use the previously used compiler flags. Fugly but works I think (for me at least).

--- Pilot_Episode/build.sh
+++ Pilot_Episode/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-additional_flags=""
+os_name=$(uname)
+
+if [[ $os_name == *"Darwin"* ]]; then
+  additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal.1.23.1"
+else
+  additional_flags=""
+fi
 #additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal.1.23.1"
 #additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib /opt/homebrew/opt/openal-soft/lib/libopenal.dylib"
 
