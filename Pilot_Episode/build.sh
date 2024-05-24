e11b947e2fc408b36881f3d010d847ea8ff877c2
Rasmus Anthin <scratty@gmail.com>
2024-05-24 21:00:35 +0200

    build.sh: * Moving the export command to the inside of the else sentence so that it does it for non mac-systems (it doesn't seem to work on mac, I think at least). This gets rid of a warning related to that. * Removing commented out additional_flags code that is not used.

--- Pilot_Episode/build.sh
+++ Pilot_Episode/build.sh
@@ -6,10 +6,7 @@ if [[ $os_name == *"Darwin"* ]]; then
   additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal.1.23.1"
 else
   additional_flags=""
+  export BUILD_PKG_CONFIG_MODULES='openal'
 fi
-#additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib -lopenal.1.23.1"
-#additional_flags="-I/opt/homebrew/opt/openal-soft/include -L/opt/homebrew/opt/openal-soft/lib /opt/homebrew/opt/openal-soft/lib/libopenal.dylib"
-
-export BUILD_PKG_CONFIG_MODULES='openal'
 
 ../../lib/Core/build.sh pilot_episode "$1" "${additional_flags[@]}"
