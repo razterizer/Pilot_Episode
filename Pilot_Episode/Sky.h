4e83541843f2a1f2e97b45a58b73abdfbc6b6a9b
Rasmus Anthin <scratty@gmail.com>
2024-06-13 00:03:14 +0200

    Sky.h: * Fixing bugs in sky color overlay drawing due to change in constructor signature of struct ttl::Rectangle.

--- Pilot_Episode/Sky.h
+++ Pilot_Episode/Sky.h
@@ -4,22 +4,23 @@
 template<int NR, int NC>
 void draw_sky(SpriteHandler<NR, NC>& sh)
 {
+  auto sky_bb = ttl::Rectangle { 1, 1, 27, 77 };
   //if (alt_km >= 85) // End of mesosphere.
   if (plane_data::blackout_state == plane_data::BlackoutState::WarnIn
     || plane_data::blackout_state == plane_data::BlackoutState::WarnOut)
   {
-    sh.replace_bg_color(Text::Color::Transparent, Text::Color::DarkRed, { 1, 1, 77, 27 });
-    sh.replace_bg_color(Text::Color::Transparent2, Text::Color::DarkRed, { 1, 1, 77, 27 });
+    sh.replace_bg_color(Text::Color::Transparent, Text::Color::DarkRed, sky_bb);
+    sh.replace_bg_color(Text::Color::Transparent2, Text::Color::DarkRed, sky_bb);
   }
   else if (plane_data::blackout_state == plane_data::BlackoutState::Stall)
   {
-    sh.replace_bg_color(Text::Color::Transparent, Text::Color::DarkYellow, { 1, 1, 77, 27 });
-    sh.replace_bg_color(Text::Color::Transparent2, Text::Color::DarkYellow, { 1, 1, 77, 27 });
+    sh.replace_bg_color(Text::Color::Transparent, Text::Color::DarkYellow, sky_bb);
+    sh.replace_bg_color(Text::Color::Transparent2, Text::Color::DarkYellow, sky_bb);
   }
   else if (plane_data::blackout_state == plane_data::BlackoutState::Blackout)
   {
-    sh.replace_fg_color(Text::Color::Black, { 1, 1, 77, 27 });
-    sh.replace_bg_color(Text::Color::Black, { 1, 1, 77, 27 });
+    sh.replace_fg_color(Text::Color::Black, sky_bb);
+    sh.replace_bg_color(Text::Color::Black, sky_bb);
   }
   else if (plane_data::alt_ft > alt_soft_limit_ft) // A cessna can fly up to a height of 51'000 ft.
   {
@@ -27,14 +28,14 @@ void draw_sky(SpriteHandler<NR, NC>& sh)
     auto t = (plane_data::alt_ft - alt_soft_limit_ft)/(alt_hard_limit_ft - alt_soft_limit_ft);
     if (t > 1) t = 1;
     int h_offs = math::roundI(27*t);
-    sh.replace_bg_color(Text::Color::Transparent, Text::Color::DarkBlue, { 1, 1, 77, h_offs });
-    sh.replace_bg_color(Text::Color::Transparent2, Text::Color::DarkBlue, { 1, 1, 77, h_offs });
+    sh.replace_bg_color(Text::Color::Transparent, Text::Color::DarkBlue, { 1, 1, h_offs, 77 });
+    sh.replace_bg_color(Text::Color::Transparent2, Text::Color::DarkBlue, { 1, 1, h_offs, 77 });
     if (plane_data::alt_km >= 100) // Kármán line.
     {
       auto t = (plane_data::alt_km - 100)/(400 - 100);
       if (t > 1) t = 1;
       int h_offs = math::roundI(27*t);
-      sh.replace_bg_color(Text::Color::DarkBlue, Text::Color::Black, { 1, 1, 77, h_offs });
+      sh.replace_bg_color(Text::Color::DarkBlue, Text::Color::Black, { 1, 1, h_offs, 77 });
     }
   }
 }
