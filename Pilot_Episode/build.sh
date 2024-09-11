6fb948e0e37462116f3474afe1aee538e3cc48d6
Rasmus Anthin <32767250+razterizer@users.noreply.github.com>
2024-09-11 09:09:44 +0200

    Update build.sh
    Hopefully properly propagates the error code from Core/build.sh.

--- Pilot_Episode/build.sh
+++ Pilot_Episode/build.sh
@@ -12,6 +12,14 @@ fi
 
 ../../lib/Core/build.sh pilot_episode "$1" "${additional_flags[@]}"
 
+# Capture the exit code of Core/build.sh
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Core/build.sh failed with exit code $exit_code"
+  exit $exit_code
+fi
+
 ### Post-Build Actions ###
 
 mkdir -p bin/fonts/
