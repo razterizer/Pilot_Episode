0432b524bc8ada856d9ef7ba5c8a49d74c3a6f54
Rasmus Anthin <scratty@gmail.com>
2024-09-17 22:54:34 +0200

    Adding build script for windows. Hopefully works.

---
+++ Pilot_Episode/build.bat
@@ -0,0 +1,15 @@
+call ..\..\..\lib\Core\build.bat
+ 
+REM Compile the demo using VC++
+cl /std:c++20 /EHsc /Fe:bin/pilot_episode.exe /Fo:bin/pilot_episode.obj .\pilot_episode.cpp /I../..
+
+if %errorlevel% neq 0 (
+    echo Build failed with error code %errorlevel%.
+    exit /b %errorlevel%
+)
+ 
+REM Create necessary directories and copy resources
+if not exist bin\fonts mkdir bin\fonts
+xcopy /Y /E ..\..\..\lib\Termin8or\fonts bin\fonts
+
+xcopy /Y /E chiptune2.ct bin
