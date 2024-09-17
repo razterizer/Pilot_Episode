86e06aedb08b978f43b8e49fe2615e1f051cf3d9
Rasmus Anthin <scratty@gmail.com>
2024-09-17 23:10:28 +0200

    build.bat: * Oops. Also forgot to add the 3rdparty/lib folder to the cl command too.

--- Pilot_Episode/build.bat
+++ Pilot_Episode/build.bat
@@ -1,7 +1,7 @@
 call ..\..\lib\Core\build.bat
  
 REM Compile the demo using VC++
-cl /std:c++20 /EHsc /Fe:bin/pilot_episode.exe /Fo:bin/pilot_episode.obj .\pilot_episode.cpp /I..\..\lib /I..\..\3rdparty\include
+cl /std:c++20 /EHsc /Fe:bin/pilot_episode.exe /Fo:bin/pilot_episode.obj .\pilot_episode.cpp /I..\..\lib /I..\..\3rdparty\include /L..\..\3rdparty\lib
 
 if %errorlevel% neq 0 (
     echo Build failed with error code %errorlevel%.
