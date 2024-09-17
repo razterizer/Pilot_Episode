ccc638c3db566bccb1d6e0b64681a83319e24a58
Rasmus Anthin <scratty@gmail.com>
2024-09-17 23:00:10 +0200

    build.bat: * Used one ../ too many.

--- Pilot_Episode/build.bat
+++ Pilot_Episode/build.bat
@@ -1,7 +1,7 @@
-call ..\..\..\lib\Core\build.bat
+call ..\..\lib\Core\build.bat
  
 REM Compile the demo using VC++
-cl /std:c++20 /EHsc /Fe:bin/pilot_episode.exe /Fo:bin/pilot_episode.obj .\pilot_episode.cpp /I..\..\..\lib
+cl /std:c++20 /EHsc /Fe:bin/pilot_episode.exe /Fo:bin/pilot_episode.obj .\pilot_episode.cpp /I..\..\lib
 
 if %errorlevel% neq 0 (
     echo Build failed with error code %errorlevel%.
@@ -10,6 +10,6 @@ if %errorlevel% neq 0 (
  
 REM Create necessary directories and copy resources
 if not exist bin\fonts mkdir bin\fonts
-xcopy /Y /E ..\..\..\lib\Termin8or\fonts bin\fonts
+xcopy /Y /E ..\..\lib\Termin8or\fonts bin\fonts
 
 xcopy /Y /E chiptune2.ct bin
