4a75e9cb3aa6f7ff63ee684091fb66e5d484ed40
Rasmus Anthin <scratty@gmail.com>
2024-09-19 17:34:05 +0200

    build.bat: * Using msbuild instead of manually compiling and linking.

--- Pilot_Episode/build.bat
+++ Pilot_Episode/build.bat
@@ -1,15 +1,15 @@
+@echo off
 call ..\..\lib\Core\build.bat
- 
-REM Compile the demo using VC++
-cl /std:c++20 /EHsc /Fe:bin/pilot_episode.exe /Fo:bin/pilot_episode.obj .\pilot_episode.cpp /I..\..\lib /I..\..\3rdparty\include /L..\..\3rdparty\lib
+
+SET configuration="Release"
+IF "%~1" == "Debug" SET configuration="Debug"
+SET target="x64"
+IF "%~2" == "x86" SET target="x86"
+msbuild Pilot_Episode.sln /p:Configuration=%configuration% /p:Platform=%target%
 
 if %errorlevel% neq 0 (
-    echo Build failed with error code %errorlevel%.
+    echo Compilation failed with error code %errorlevel%.
     exit /b %errorlevel%
 )
- 
-REM Create necessary directories and copy resources
-if not exist bin\fonts mkdir bin\fonts
-xcopy /Y /E ..\..\lib\Termin8or\fonts bin\fonts
 
-xcopy /Y /E chiptune2.ct bin
+echo Build succeeded.
