e8a50071b919757c9d317c1695e652d9ccbe4762
Rasmus Anthin <scratty@gmail.com>
2024-10-01 23:15:07 +0200

    Sun.h: * windows warnings fixes.

--- Pilot_Episode/Sun.h
+++ Pilot_Episode/Sun.h
@@ -5,13 +5,14 @@ template<int NR, int NC>
 void draw_sun(SpriteHandler<NR, NC>& sh, int r, int c, float x_pos)
 {
   const float parallax = 5e-3f;
-  sh.write_buffer("   ", r, c + 2 + parallax*x_pos, Color::DarkYellow, Color::Yellow);
-  sh.write_buffer("       ", r + 1, c + parallax*x_pos, Color::DarkYellow, Color::Yellow);
-  sh.write_buffer("       ", r + 2, c + parallax*x_pos, Color::DarkYellow, Color::Yellow);
-  sh.write_buffer("   ", r + 3, c + 2 + parallax*x_pos, Color::DarkYellow, Color::Yellow);
-  sh.write_buffer(" ", r, c + 1 + parallax*x_pos, Color::DarkYellow, Color::Yellow);
-  sh.write_buffer(" ", r, c + 5 + parallax*x_pos, Color::DarkYellow, Color::Yellow);
-  sh.write_buffer(" ", r + 3, c + 1 + parallax*x_pos, Color::DarkYellow, Color::Yellow);
-  sh.write_buffer(" ", r + 3, c + 5 + parallax*x_pos, Color::DarkYellow, Color::Yellow);
+  auto c_offs = static_cast<int>(parallax*x_pos);
+  sh.write_buffer("   ", r, c + 2 + c_offs, Color::DarkYellow, Color::Yellow);
+  sh.write_buffer("       ", r + 1, c + c_offs, Color::DarkYellow, Color::Yellow);
+  sh.write_buffer("       ", r + 2, c + c_offs, Color::DarkYellow, Color::Yellow);
+  sh.write_buffer("   ", r + 3, c + 2 + c_offs, Color::DarkYellow, Color::Yellow);
+  sh.write_buffer(" ", r, c + 1 + c_offs, Color::DarkYellow, Color::Yellow);
+  sh.write_buffer(" ", r, c + 5 + c_offs, Color::DarkYellow, Color::Yellow);
+  sh.write_buffer(" ", r + 3, c + 1 + c_offs, Color::DarkYellow, Color::Yellow);
+  sh.write_buffer(" ", r + 3, c + 5 + c_offs, Color::DarkYellow, Color::Yellow);
 }
 
