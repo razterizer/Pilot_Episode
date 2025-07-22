0d31149ecf0ea59a0d7b4fcabe56e213a362532b
Rasmus Anthin <scratty@gmail.com>
2025-07-22 14:40:13 +0800

    Adding comment.

--- Pilot_Episode/pilot_episode.cpp
+++ Pilot_Episode/pilot_episode.cpp
@@ -489,6 +489,7 @@ int main(int argc, char** argv)
     {
       std::cout << "demo --help | [--log_mode (record | replay)] [--suppress_tty_output] [--suppress_tty_input] [--altitude_start_km <altitude_km>] [--disable_altitude_limiting] [--set_fps <fps>] [--set_sim_delay_us <delay_us>]" << std::endl;
       std::cout << "  default values:" << std::endl;
+      // Will unfortunately report the wrong default value for <altitude_km> if ordering the arguments like this "--altitude_start_km <altitude_km> --help".
       std::cout << "    <altitude_km> : " << game.get_alt_km() << std::endl;
       std::cout << "    <fps>         : " << game.get_real_fps() << std::endl;
       std::cout << "    <delay_us>    : " << game.get_sim_delay_us() << std::endl;
