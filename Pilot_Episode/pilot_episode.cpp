f09ef73dfb95ed77456ca7d65df7ce227e872dd8
Rasmus Anthin <scratty@gmail.com>
2025-09-12 16:47:07 +0200

    Updated the xcode_log_path.

--- Pilot_Episode/pilot_episode.cpp
+++ Pilot_Episode/pilot_episode.cpp
@@ -492,7 +492,7 @@ int main(int argc, char** argv)
         params.log_mode = LogMode::Record;
       else if (std::strcmp(argv[i + 1], "replay") == 0)
         params.log_mode = LogMode::Replay;
-      params.xcode_log_path = "../../../../../../../../Documents/xcode/Pilot_Episode/Pilot_Episode";
+      params.xcode_log_path = "../../../../../../../../Documents/xcode/Pilot_Episode/Pilot_Episode/bin/";
     }
   }
   
